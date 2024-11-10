12c283fa129c33284e55596b235d325abb954c74
George-Dsss <ajithgeorge0303@gmail.com>
2024-11-10 23:07:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="dsss_homework_2",  # Replace with your project's name
+    name="dsss_hmwrk_2",  # Replace with your project's name
     version="0.1.0",  # Replace with the appropriate version
-    author="Hari",  # Replace with the author's name
-    author_email="haripvtt@gmail.com",  # Replace with the author's email
+    author="Ajith",  # Replace with the author's name
+    author_email="ajithgeorge0303@gmail.com",  # Replace with the author's email
     description="A brief description of your project",  # Short description of your package
     long_description=open("README.md").read(),  # Long description from a README file (optional)
     long_description_content_type="text/markdown",  # If using Markdown for README
-    url="https://github.com/username/dsss_homework_2",  # Replace with the URL of your project (e.g., GitHub)
+    url="https://github.com/George-Dsss/dsss_hmwrk_2",  # Replace with the URL of your project (e.g., GitHub)
     packages=find_packages(),  # Automatically find all the packages in your project
     classifiers=[  # Optional, list of classifiers to categorize your project on PyPI
         "Programming Language :: Python :: 3",
